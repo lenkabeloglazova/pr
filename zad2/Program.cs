858f906e0adeda517068afb6049db789ab10933b
Elena <lenkabeloglazova@yandex.ru>
2023-03-12 14:34:14 +0300

    add zad2

---
+++ zad2/Program.cs
@@ -0,0 +1,31 @@
+﻿// // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// // M = 1; N = 15 -> 120
+// // M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+// вызов функции "сумма чисел от M до N"
+void SumFromMToN(int m, int n)
+{
+    Console.Write (SumMN (m - 1, n));
+}
+
+// функция сумма чисел от M до N
+int SumMN(int m, int n)
+{
+    int= m;
+    if (m == n)
+        0 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
